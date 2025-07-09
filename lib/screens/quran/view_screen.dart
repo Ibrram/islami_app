b266bf0fc3cec64a40c5ec721a86abdee9843cd5
Ibram <9iro@bk.ru>
2025-07-09 21:01:53 +0300

    initialize a screen to view the verses of Quarn [selected sura]

---
+++ lib/screens/quran/view_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:islami_app_task/models/sura_model.dart';
+
+class ViewScreen extends StatelessWidget {
+  final SuraModel sura;
+
+  const ViewScreen({
+    super.key,
+    required this.sura,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
