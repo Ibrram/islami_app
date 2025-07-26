2e7cedf740e94de79bf546550cff9799790b7f41
Ibram <9iro@bk.ru>
2025-07-26 05:10:14 +0300

    Onboard add scroll for small devices

--- lib/screens/onboarding/widgets/card_of_image_text.dart
+++ lib/screens/onboarding/widgets/card_of_image_text.dart
@@ -13,39 +13,41 @@ class CardOfImageText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Image.asset(imagePath),
-        SizedBox(
-          height:
-              MediaQuery.of(context).size.height *
-              ((textTwo ?? '').isEmpty ? 0.085 : 0.042),
-        ),
-        Text(
-          textOne,
-          // textAlign: TextAlign.center,
-          style: const TextStyle(
-            color: Color(0xFFE2BE7F),
-            fontFamily: 'Janna',
-            fontWeight: FontWeight.w700,
-            fontSize: 24,
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(imagePath),
+          SizedBox(
+            height:
+                MediaQuery.of(context).size.height *
+                ((textTwo ?? '').isEmpty ? 0.085 : 0.042),
           ),
-        ),
-        if ((textTwo ?? '').isNotEmpty) ...[
-          SizedBox(height: MediaQuery.of(context).size.height * 0.042),
           Text(
-            textTwo!,
-            textAlign: TextAlign.center,
+            textOne,
+            // textAlign: TextAlign.center,
             style: const TextStyle(
               color: Color(0xFFE2BE7F),
               fontFamily: 'Janna',
               fontWeight: FontWeight.w700,
-              fontSize: 20,
+              fontSize: 24,
             ),
           ),
+          if ((textTwo ?? '').isNotEmpty) ...[
+            SizedBox(height: MediaQuery.of(context).size.height * 0.042),
+            Text(
+              textTwo!,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: Color(0xFFE2BE7F),
+                fontFamily: 'Janna',
+                fontWeight: FontWeight.w700,
+                fontSize: 20,
+              ),
+            ),
+          ],
         ],
-      ],
+      ),
     );
   }
 }
